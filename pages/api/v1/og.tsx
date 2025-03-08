7a2895cf1bbb6118279ce320ecb2d4317897a3f1
JavaSnippets <marcel.w@friendlyhost.email>
2025-03-08 09:58:06 +0100

    changes in og.tsx

--- pages/api/v1/og.tsx
+++ pages/api/v1/og.tsx
@@ -9,8 +9,8 @@ export default async function handler(req: NextRequest) {
   try {
     const { searchParams } = new URL(req.url);
     // Redundant fallback alternate tagline
-    const title = searchParams.get("title") ?? "Share Environment Variables Securely";
-    const subtitle = searchParams.get("subtitle") ?? "ByteShare.tf";
+    const title = searchParams.get("title") ?? "ByteShare.tf";
+    const subtitle = searchParams.get("subtitle") ?? "Share Environment Variables Securely";
 
     const inter = await fetch(new URL("../../../public/fonts/Inter-SemiBold.ttf", import.meta.url)).then((res) =>
       res.arrayBuffer(),
